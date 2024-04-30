f7f1829925e215bf11150ddbbb0e6c5e77ed908d
Anton Malynevskyi <malina.elfg@gmail.com>
2024-04-30 09:26:03 +0200

    Input

--- functions.c
+++ functions.c
@@ -3,3 +3,87 @@
 //
 
 #include "functions.h"
+
+category input_category(int choice) {
+    category result;
+    switch (choice) {
+        case 1:
+            result = ART_NOVEL;
+            break;
+        case 2:
+            result = DICTIONARY;
+            break;
+        case 3:
+            result = TEXTBOOK;
+            break;
+        default:
+            puts("Error");
+            result = ERROR;
+    }
+    return result;
+}
+
+void input_union(book *result) {
+    char temp[100];
+    switch (result->category) {
+        case ART_NOVEL:
+            puts("Input genre");
+            gets(temp);
+            strcpy(result->details.art_novel_details.genre = Mem(strlen(temp)) + 1, temp);
+            puts("Input illustrator's lastname");
+            gets(temp);
+            strcpy(result->details.art_novel_details.illustrator = Mem(strlen(temp)) + 1, temp);
+            break;
+        case DICTIONARY:
+            puts("Input number of words");
+            scanf("%d", result->details.dictionaryWordCount);
+            break;
+        case TEXTBOOK:
+            puts("Input subject");
+            gets(temp);
+            strcpy(result->details.text_textbook_details.subject = Mem(strlen(temp)) + 1, temp);
+            puts("Input grade");
+            scanf("%d", &result->details.text_textbook_details.grade);
+            break;
+    }
+}
+
+book input_book() {
+    puts("Input category:\n1) Art Novel\n2) Dictionary\n3) Textbook");
+    int choice;
+    scanf("%d", &choice);
+    book result;
+    result.category = input_category(choice);
+    if (result.category != ERROR) {
+        char temp[100];
+        puts("Input title");
+        gets(temp);
+        strcpy(result.title = Mem(strlen(temp)) + 1, temp);
+        puts("Input Author(first name and last name");
+        gets(temp);
+        strcpy(result.author.firstName = Mem(strlen(temp)) + 1, temp);
+        gets(temp);
+        strcpy(result.author.lastName = Mem(strlen(temp)) + 1, temp);
+        puts("Input price");
+        scanf("%f", &result.price);
+        puts("Input release year");
+        scanf("%d", &result.year);
+        input_union(&result);
+    } else {
+        puts("Wrong input");
+    }
+    return result;
+}
+
+array input_array(int size) {
+    array result;
+    result.catalog = (book *) malloc(size * sizeof(book));
+    result.size = size;
+    for (int i = 0; i < result.size; ++i) {
+        result.catalog[i] = input_book();
+        if (result.catalog[i].category == ERROR) {
+            i--;
+        }
+    }
+    return result;
+}
--- functions.h
+++ functions.h
@@ -5,29 +5,36 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-typedef enum Category {
+#define Mem(n) (char*)malloc(n*sizeof(char))
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum category {
     ART_NOVEL, // Художня література
     DICTIONARY, // Словник
-    TEXTBOOK // Підручник
-} Category;
+    TEXTBOOK, // Підручник
+    ERROR
+} category;
 
- typedef struct ArtNovelDetails {
-    char genre[30]; // Жанр
-    char illustrator[30]; // Прізвище художника-ілюстратора
+ typedef struct artNovelDetails {
+    char *genre; // Жанр
+    char *illustrator; // Прізвище художника-ілюстратора
 } artNovelDetails;
 
-typedef struct TextbookDetails {
-    char subject[30]; // Предметна галузь
+typedef struct textbookDetails {
+    char *subject; // Предметна галузь
     int grade; // Клас
 } textbookDetails;
 
-typedef union BookDetails {
+typedef union bookDetails {
     artNovelDetails art_novel_details;
     int dictionaryWordCount; // Кількість слів у словнику
     textbookDetails text_textbook_details;
-} BookDetails;
+} bookDetails;
 
-typedef struct Book {
+typedef struct book {
     char *title; // Назва
     struct Author {
         char *firstName; // Ім'я автора
@@ -35,8 +42,35 @@ typedef struct Book {
     } author;
     float price; // Ціна
     int year; // Рік видання
-    Category category; // Категорія
-    BookDetails details; // Деталі книги
-} Book;
+    category category; // Категорія
+    bookDetails details; // Деталі книги
+} book;
+
+typedef struct array {
+    book *catalog;
+    int size;
+}array;
+
+category input_category(int choice);
+
+void input_union(book*);
+
+book input_book();
+
+array input_array(int);
+
+void output_catagory(book);
+
+void output_book(book);
+
+void output_union(book);
+
+void output_array(array);
+
+array find_by_category(array, category);
+
+array find_by_detail(array, category, char*);
+
+array find_by_price(array, int max, int min);
 
 #endif //FUNCTIONS_H
